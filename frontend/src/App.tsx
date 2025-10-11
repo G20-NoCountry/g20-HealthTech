536738eb3922d04958a86bce5c9af015ebdc057f
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-10-11 14:28:07 -0500

    Creating route components, installing usenavigate, browseRouter, common-RouterLayout folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import { useRef } from 'react';
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
+import { BrowserRouter } from "react-router-dom";
+import { Toast } from "primereact/toast";
+import { Button } from "primereact/button";
+import { useRef } from "react";
+import { AppRouter } from "./routes/router";
 
-function App() {
+const App = () => {
   const toast = useRef<Toast>(null);
 
   const show = () => {
-    toast.current?.show({ severity: 'info', summary: 'Info', detail: 'Message Content' });
+    toast.current?.show({
+      severity: "info",
+      summary: "Info",
+      detail: "Message Content",
+    });
   };
 
   return (
-    <div className="flex min-h-dvh items-center justify-center">
-      <Toast ref={toast} />
-      <Button onClick={show} label="Show" />
-    </div>
+    <BrowserRouter>
+      <div className="flex min-h-dvh items-center justify-center">
+        <Toast ref={toast} />
+        <Button onClick={show} label="Show" />
+        <AppRouter />
+      </div>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;

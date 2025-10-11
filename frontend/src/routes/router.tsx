58d86d14ea49a452d8bc6fdb55f676d9b26c3d1b
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-10-11 16:03:00 -0500

    delete Browse Router and react router dom

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
-
 import { RouterLayout } from './common/RouterLayout.tsx';
-
+import { Route, Routes } from 'react-router';
+import { Login } from '../pages/Login.tsx';
 
 export const AppRouter: FC = () => {
   return (

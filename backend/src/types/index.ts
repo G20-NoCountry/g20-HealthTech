7c702c838c43c7c61a41056f5a80858f1331ec1e
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-10-13 10:24:47 -0300

    feat: added types

---
+++ backend/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Medic {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  gender: string;
+  image: string;
+  especialidad: string;
+  matricula: number;
+  telefono: number;
+}

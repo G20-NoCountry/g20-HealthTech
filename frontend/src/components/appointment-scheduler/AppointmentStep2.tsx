54a1214d0b54fab0c535e2f28e9e7e2339335ef7
lorenzosegada <lorenzosegada@gmail.com>
2025-10-16 19:23:53 -0300

    syle: Mejora el estilo del scroll en la selección de horarios

--- frontend/src/components/appointment-scheduler/AppointmentStep2.tsx
+++ frontend/src/components/appointment-scheduler/AppointmentStep2.tsx
@@ -5,9 +5,15 @@ interface Step2Props { onNext: () => void; onPrev: () => void; }
 
 export const AppointmentStep2= ({  onNext, onPrev }: Step2Props) => {
     const availableTimes = [
-        '9:00 AM', '10:00 AM', '11:30 AM', '12:30 PM', 
-        '1:45 PM', '2:00 PM', '3:30 PM', '4:00 PM', 
-        '4:30 PM', '5:00 PM', '5:30 PM', '5:00 PM', '5:30 PM'
+        '9:00 AM',  '9:15 AM',  '9:30 AM',  '9:45 AM',
+    '10:00 AM', '10:15 AM', '10:30 AM', '10:45 AM',
+    '11:00 AM', '11:15 AM', '11:30 AM', '11:45 AM',
+    '12:00 PM', '12:15 PM', '12:30 PM', '12:45 PM',
+    '1:00 PM',  '1:15 PM',  '1:30 PM',  '1:45 PM',
+    '2:00 PM',  '2:15 PM',  '2:30 PM',  '2:45 PM',
+    '3:00 PM',  '3:15 PM',  '3:30 PM',  '3:45 PM',
+    '4:00 PM',  '4:15 PM',  '4:30 PM',  '4:45 PM',
+    '5:00 PM'
       ];
 
   return (
@@ -25,13 +31,15 @@ export const AppointmentStep2= ({  onNext, onPrev }: Step2Props) => {
                 </div>
             </div>
             <div className="flex-1">
-                <label className="font-semibold block mb-2">HORA DISPONIBLE</label>
-                <div className="grid grid-cols-3 gap-3">
-                {availableTimes.map((time) => (
-                    <button key={time} className="w-full p-3 border border-gray-300 rounded-lg text-center text-gray-700 hover:border-[#734F96] hover:text-[#734F96] focus:bg-purple-50 focus:border-[#734F96] focus:text-[#734F96] transition-colors">
-                        {time}
-                    </button>
-                ))}
+              <label className="font-semibold block mb-2">HORA DISPONIBLE</label>
+                <div className='h-72 overflow-y-auto pr-2'>
+                    <div className="grid grid-cols-3 gap-3">
+                      {availableTimes.map((time) => (
+                       <button key={time} className="w-full p-3 border border-gray-300 rounded-lg text-center text-gray-700 hover:border-[#734F96] hover:text-[#734F96] focus:bg-purple-50 focus:border-[#734F96] focus:text-[#734F96] transition-colors">
+                          {time}
+                      </button>
+                      ))}
+                  </div>  
                 </div>
             </div>
         </div>

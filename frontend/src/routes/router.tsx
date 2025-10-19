7b442e4980d654dccd6ac329b2450bec3c942421
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-10-18 23:16:32 -0500

    feature: realice el componente register siguiendo la misma estructura de creacion del login

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,14 +1,19 @@
-import { RouterLayout } from './common/RouterLayout.tsx';
+// src/routes/router.tsx
+
+// Eliminamos 'import React from "react";'
 import { Route, Routes } from 'react-router';
-import { LoginPage } from '../pages/LoginPage.tsx';
+import { LoginPage } from '../pages/LoginPage.tsx'; // Asumo esta ruta
+import { RegisterPage } from '../pages/RegisterPage.tsx';
+import { RouterLayout } from './common/RouterLayout.tsx'; // Importa la página corregida
 
-
-export const AppRouter= () => {
+// export const de la aplicación principal
+export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<RouterLayout />}>
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
+      <Route path="/" element={<RouterLayout />} />
+      <Route path="/login" element={<LoginPage />} />
+      {/* Uso de la página corregida */}
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 };

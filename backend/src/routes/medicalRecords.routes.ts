5a8473d6a4ac11e5b65a804e3b498c4ac544c594
finstory <facu995electro@hotmail.com>
2025-10-26 18:25:42 -0300

    fix/canAccessMedic

--- backend/src/routes/medicalRecords.routes.ts
+++ backend/src/routes/medicalRecords.routes.ts
@@ -3,11 +3,13 @@ import { MedicalRecordController } from '../controllers/medicalRecord.controller
 import { isAdmin } from '../middlewares/auth/admin.middleware';
 import { patientIdValidator } from '../validators/param/patientId.validator';
 import { isAuthenticated } from '../middlewares/auth/authenticated.middleware';
+import { canAccessMedic } from '../middlewares/user/canAccessMedic.middleware';
 
 const router = Router();
 const medicalRecordController = new MedicalRecordController;
 router.use(isAuthenticated);
 
-router.get('/medical_records/:patient_id', patientIdValidator, isAdmin, medicalRecordController.getMedicalRecord);
+//$ [FIX] - Solo médicos pueden acceder a los historiales médicos??.
+router.get('/medical_records/:patient_id', canAccessMedic, patientIdValidator, medicalRecordController.getMedicalRecord);
 
 export default router;

d3e867df18082a1fe478ff812e7805e269e0f2bc
lorenzosegada <lorenzosegada@gmail.com>
2025-10-26 01:27:07 -0300

    feat: Añadir modal en la parte de diagnostico

--- frontend/src/components/DasboardMedico/NextAppointmentCard.tsx
+++ frontend/src/components/DasboardMedico/NextAppointmentCard.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router";
-import { nextAppointment } from "./mockDoctorData";
+import { useState } from 'react';
+import { Dialog } from 'primereact/dialog';
+import { DiagnosisModal } from './DiagnosisModal';
 
-interface NextAppointment {
+export interface NextAppointment {
   patientName: string;
   reason: string;
   time: string;
   isToday: boolean;
-  meetLink?: string; //  un link para la videollamada
-  patientId?: string; //  un ID para ir al perfil
+  meetLink?: string; 
+  patientId?: string; 
 }
 
 interface NextAppointmentCardProps {
@@ -16,7 +18,12 @@ interface NextAppointmentCardProps {
 
 export const NextAppointmentCard = ({ appointment }: NextAppointmentCardProps) => {
   const navigate = useNavigate();
-  // Botón "Ver Perfil"
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleDiagnostico = () => {
+    setIsModalVisible(true);
+  };
+
   const handleVerPerfil = () => {
     if (appointment.patientId) {
       navigate(`/pacientes/${appointment.patientId}`);
@@ -26,32 +33,46 @@ export const NextAppointmentCard = ({ appointment }: NextAppointmentCardProps) =
   };
 
   return (
-    <div className="bg-[#734F96]  p-6 rounded-3xl border   flex flex-col md:flex-row justify-between items-center ">
-              <div className="mb-4 md:mb-0 text-white">
-                <p className="text-sm uppercase">Iniciar Video Llamada - Próxima Cita -</p>
-                <p className="text-2xl font-bold">Hoy - {nextAppointment.time}</p>
-                <p className="text-lg">{nextAppointment.patientName} ({nextAppointment.reason})</p>
-              </div>
-              <div className="flex flex-col gap-2 w-full md:w-auto">
-                <button className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
-                    <i className="pi pi-folder-open "></i>
-                    <span>DIAGNOSTICO</span>
-                </button>
-                 <button 
-                 onClick={handleVerPerfil}
-                 className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
-                <i className="pi pi-user "></i>               
-                     <span>VER PERFIL</span>
-                </button>   
-                    <a 
-                        href={appointment.meetLink || "https://meet.google.com/landing"} 
-                         target="_blank" 
-                         rel="noopener noreferrer"
-                        className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
-                         <i className="pi pi-video"></i>
-                     <span>INICIAR AHORA</span>
-                </a>   
-            </div>
-            </div>
+    <><div className="bg-[#734F96]  p-6 rounded-3xl border   flex flex-col md:flex-row justify-between items-center ">
+      <div className="mb-4 md:mb-0 text-white">
+        <p className="text-sm uppercase">Iniciar Video Llamada - Próxima Cita -</p>
+        <p className="text-2xl font-bold">Hoy - {appointment.time}</p>
+        <p className="text-lg">{appointment.patientName} ({appointment.reason})</p>
+      </div>
+
+      <div className="flex flex-col gap-2 w-full md:w-auto">
+        <button
+          onClick={handleDiagnostico}
+          className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
+          <i className="pi pi-folder-open "></i>
+          <span>DIAGNOSTICO</span>
+        </button>
+
+        <button
+          onClick={handleVerPerfil}
+          className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
+          <i className="pi pi-user "></i>
+          <span>VER PERFIL</span>
+        </button>
+        <a
+          href={appointment.meetLink || "https://meet.google.com/landing"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className='bg-[#EABAFF] flex items-center justify-center gap-2  rounded-full px-5 py-3 hover:bg-gray-200 transition-colors'>
+          <i className="pi pi-video"></i>
+          <span>INICIAR AHORA</span>
+        </a>
+      </div>
+    </div>
+    <Dialog
+      header="Registro de Consulta"
+      visible={isModalVisible}
+      style={{ width: '50vw' }} 
+      onHide={() => setIsModalVisible(false)}
+    >
+        <DiagnosisModal
+          appointmentData={appointment}
+          onClose={() => setIsModalVisible(false)} />
+      </Dialog></>
   );
 };
